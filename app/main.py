771847862a7cfa0deddfcefae1df299a913513d5
m3 <sergey.radik@gmail.com>
2026-06-18 15:26:37 +0300

    feat. complete weather service dockerization and add pull command

--- app/main.py
+++ app/main.py
@@ -1,6 +1,37 @@
+#command=docker pull rer8/weather:latest
+
+import os
+import requests
+
+URL = "https://api.weatherapi.com/v1/current.json"
+FILTERING = "Paris"
+API_KEY = os.getenv("API_KEY")
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    if not API_KEY:
+        print("Error: API_KEY environment variable is missing!")
+        return
+
+    try:
+        response = requests.get(
+            URL,
+            params={
+                "key": API_KEY,
+                "q": FILTERING
+            }
+        )
+        response.raise_for_status()
+
+        data = response.json()
+        city = data["location"]["name"]
+        temp = data["current"]["temp_c"]
+        condition = data["current"]["condition"]["text"]
+
+        print(f"Current weather in {city}: {temp}°C, {condition}")
+
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred while fetching weather data: {e}")
 
 
 if __name__ == "__main__":
